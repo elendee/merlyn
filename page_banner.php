7bb92d8cfa4ae3017b64e3dd3d001ef887b01ddc
Kerry O'Connor <kerryoco@gmail.com>
2020-09-25 21:04:37 -0400

    header

--- page_banner.php
+++ page_banner.php
@@ -7,9 +7,15 @@ if( get_field('page_banner') ){
 
 	if( $desktop_height ){
 		echo '<div class="page-banner desktop-only" style="background-image: url(' . $banner . '); height:' . $desktop_height . 'px"></div>';
+		if( !$mobile_height ){
+			echo '<div class="page-banner mobile-only" style="background-image: url(' . $banner . '); height: 200px"></div>';
+		}
 	}
 	if( $mobile_height ){
 		echo '<div class="page-banner mobile-only" style="background-image: url(' . $banner . '); height:' . $mobile_height . 'px"></div>';
+		if( !$desktop_height ){
+			echo '<div class="page-banner mobile-only" style="background-image: url(' . $banner . '); height: 200px"></div>';
+		}
 	}
 	if( !$mobile_height && !$desktop_height ){
 		echo '<div class="page-banner" style="background-image: url(' . $banner . ');"></div>';
