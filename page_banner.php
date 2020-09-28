57a549e2f1b8fe7c5dc635b6f8812826feee0a4b
Kerry O'Connor <kerryoco@gmail.com>
2020-09-28 17:08:51 -0400

    banner

--- page_banner.php
+++ page_banner.php
@@ -1,24 +1,26 @@
 <?php 
+
 if( get_field('page_banner') ){
 
 	$banner = get_field('page_banner');
 	$desktop_height = get_field('page_banner_height_desktop');
 	$mobile_height = get_field('page_banner_height_mobile');
+	$banner_overlay = get_field('banner_overlay') ? '<div class="flex-wrapper banner-overlay"><div>' . get_field('banner_overlay') . '</div></div>' : '';
 
 	if( $desktop_height ){
-		echo '<div class="page-banner desktop-only" style="background-image: url(' . $banner . '); height:' . $desktop_height . 'px"></div>';
+		echo '<div class="page-banner desktop-only" style="background-image: url(' . $banner . '); height:' . $desktop_height . 'px"> ' . $banner_overlay . '</div>';
 		if( !$mobile_height ){
-			echo '<div class="page-banner mobile-only" style="background-image: url(' . $banner . '); height: 200px"></div>';
+			echo '<div class="page-banner mobile-only" style="background-image: url(' . $banner . '); height: 200px">' . $banner_overlay . '</div>';
 		}
 	}
 	if( $mobile_height ){
-		echo '<div class="page-banner mobile-only" style="background-image: url(' . $banner . '); height:' . $mobile_height . 'px"></div>';
+		echo '<div class="page-banner mobile-only" style="background-image: url(' . $banner . '); height:' . $mobile_height . 'px">' . $banner_overlay . '</div>';
 		if( !$desktop_height ){
-			echo '<div class="page-banner mobile-only" style="background-image: url(' . $banner . '); height: 200px"></div>';
+			echo '<div class="page-banner mobile-only" style="background-image: url(' . $banner . '); height: 200px">' . $banner_overlay . '</div>';
 		}
 	}
 	if( !$mobile_height && !$desktop_height ){
-		echo '<div class="page-banner" style="background-image: url(' . $banner . ');"></div>';
+		echo '<div class="page-banner" style="background-image: url(' . $banner . ');">' . $banner_overlay . '</div>';
 	}
 	
 }
